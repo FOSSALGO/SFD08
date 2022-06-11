d932e85e38d64b7f0c7e92c8891bc59320553534
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2022-06-11 23:30:02 +0800

    Add files via upload

---
+++ src/sfd/Canvas.java
@@ -0,0 +1,76 @@
+package sfd;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import javax.swing.JPanel;
+
+public class Canvas extends JPanel {
+
+    private int cellSize = 40;
+    private double translateX;
+    private double translateY;
+    private double scale;
+
+    public Canvas() {
+        translateX = 0;
+        translateY = 0;
+        scale = 1;
+        setOpaque(false);
+        setDoubleBuffered(true);
+    }
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public void setCellSize(int cellSize) {
+        this.cellSize = cellSize;
+    }
+
+    public double getTranslateX() {
+        return translateX;
+    }
+
+    public void setTranslateX(double translateX) {
+        this.translateX = translateX;
+    }
+
+    public double getTranslateY() {
+        return translateY;
+    }
+
+    public void setTranslateY(double translateY) {
+        this.translateY = translateY;
+    }
+
+    public double getScale() {
+        return scale;
+    }
+
+    public void setScale(double scale) {
+        this.scale = scale;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+        //----------------------------------------------------------------------
+        AffineTransform tx = new AffineTransform();
+        tx.translate(translateX, translateY);
+        tx.scale(scale, scale);
+        g2d.setTransform(tx);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        //----------------------------------------------------------------------
+        g2d.setColor(Color.GRAY);
+        g2d.fillRect(0, 0, 200, 100);
+
+        //----------------------------------------------------------------------
+        g2d.dispose();
+    }
+}
